23295a11da21920d61f3c39095359ffa0c47b950
LabreCoder <jvlabremachado@gmail.com>
2026-03-22 23:41:03 -0300

    Removendo exercício que estava com código e nome errados

--- Java/POO/Aula 03/Exercício_01.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class MyName{
-    public static void main(String [] args){
-        System.out.println("Digite seu nome: ");
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Olá, meu nome é " + teclado.nextLine());
-    }
-}

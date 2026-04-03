05077a4b8607a49140d3ee0ddaedad18f9d7f08b
LabreCoder <jvlabremachado@gmail.com>
2026-04-03 13:38:52 -0300

    Adicionando exercicio

---
+++ Java/POO/Aula_07/ResolucaoProblema.java
@@ -0,0 +1,7 @@
+public class ResolucaoProblema{ // irei responder com comentários esse exercício
+    public static void main(String[] args) {
+        System.out.println("Resposta do Exercício 2");
+        System.out.println("a) O programa que irá resolver o problema é o Programa A, pois caso o usuário digite logo de cara o valor de '-1', ele não irá executar o while, já o Programa B, ele imprime o primeiro valor digitado, independente dele ser '-1' ou não para depois entrar no while.");
+        System.out.println("O correto seria fazer uma inversão, colocando o 'while...do', assim seria verificado se o primeiro valor digitado é o '-1' ou não. Caso não fosse, ele iria executar o bloco dentro do 'do'.");
+    }
+}
